5203cde6415ba88debe03e042c77314f7807f64b
Chris Sutandang <chris.sutandang@gmail.com>
2024-02-19 13:42:02 +0700

    preflight false

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./code/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
+}
+
